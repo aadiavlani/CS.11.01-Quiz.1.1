c0e09cae6f4e9f87e7c5047b0037870a70c79c80
aadiavlani <aadia2026@student.cis.edu.hk>
2024-02-06 11:36:14 +0800

    CS Unit 1 IDE

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println("Enter Your Age: ");
+        age = scan.nextInt();
+        System.out.println("Enter Your Name: ");
+        firstName = scan.next();
+        System.out.println("Enter Your Favourite Food: ");
+        favouriteFood = scan.next();
+        System.out.println("First Name: " + firstName);
+        System.out.println("Age " + age);
+        System.out.println("Favourite Food: " + favouriteFood);
 
 
 
